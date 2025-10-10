6f46e3333e0fb406d0fa7b4c0789e4935e524c9b
Joshuabrainiac <156900732+Joshuabrainiac@users.noreply.github.com>
2025-10-10 23:02:47 +0100

    feat(complete): Add completion to the dropdown
    Completed the toggle for the hamburger mobile.
    
    Added a new logo also.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
-console.log("Nice one!");
+const dropDown = document.querySelector(".hamburger-menu");
+const menuBtn = document.querySelector(".menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  const displayValue = window
+    .getComputedStyle(dropDown)
+    .getPropertyValue("display");
+
+  menuBtn.classList.toggle("active");
+  if (displayValue === "none") {
+    dropDown.style.display = "block";
+    document.body.style.height = "100vh";
+    document.body.style.overflow = "hidden";
+  } else {
+    dropDown.style.display = "none";
+    document.body.style.height = "100%";
+    document.body.style.overflow = "scroll";
+  }
+
+  console.log("Confirmed");
+  console.log(displayValue);
+});
