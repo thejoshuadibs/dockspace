912c256847645af85465eebe347c69292a09a73f
Joshuabrainiac <156900732+Joshuabrainiac@users.noreply.github.com>
2025-10-11 15:36:35 +0100

    feat(video): Refactor the signup section video
    Added a lot of files including the video files
    and folder.
    
    Refactored the video code to make it always
    play automatically in repeat mode and open
    up a lightbox when the (custom) play button
    is clicked.
    
    Made the video responsive on all devices.

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,56 @@
-const dropDown = document.querySelector(".hamburger-menu");
-const menuBtn = document.querySelector(".menu-btn");
+const dropDown = document.querySelector('.hamburger-menu');
+const menuBtn = document.querySelector('.menu-btn');
+const hamburgerLinks = document.querySelectorAll('.close-hamburger');
 
-menuBtn.addEventListener("click", () => {
+hamburgerLinks.forEach((element) => {
+  element.addEventListener("click", () => {
+    dropDown.style.display = 'none';
+    document.body.style.height = '100%';
+    document.body.style.overflow = 'scroll';
+    menuBtn.classList.toggle('active')
+  })
+});
+
+menuBtn.addEventListener('click', () => {
   const displayValue = window
     .getComputedStyle(dropDown)
-    .getPropertyValue("display");
+    .getPropertyValue('display');
 
-  menuBtn.classList.toggle("active");
-  if (displayValue === "none") {
-    dropDown.style.display = "block";
-    document.body.style.height = "100vh";
-    document.body.style.overflow = "hidden";
+  menuBtn.classList.toggle('active');
+  if (displayValue === 'none') {
+    dropDown.style.display = 'block';
+    document.body.style.height = '100vh';
+    document.body.style.overflow = 'hidden';
   } else {
-    dropDown.style.display = "none";
-    document.body.style.height = "100%";
-    document.body.style.overflow = "scroll";
+    dropDown.style.display = 'none';
+    document.body.style.height = '100%';
+    document.body.style.overflow = 'scroll';
   }
-
-  console.log("Confirmed");
-  console.log(displayValue);
 });
+
+
+
+/* Video Script */
+  const openLightbox = document.getElementById('openLightbox');
+  const closeLightbox = document.getElementById('closeLightbox');
+  const lightbox = document.getElementById('lightbox');
+  const popupVideo = document.getElementById('popupVideo');
+
+  openLightbox.addEventListener('click', () => {
+    lightbox.style.display = 'flex';
+    popupVideo.currentTime = 0; // start fresh
+    popupVideo.play();
+  });
+
+  closeLightbox.addEventListener('click', () => {
+    popupVideo.pause();
+    lightbox.style.display = 'none';
+  });
+
+  // Optional: close lightbox when background is clicked
+  lightbox.addEventListener('click', (e) => {
+    if (e.target === lightbox) {
+      popupVideo.pause();
+      lightbox.style.display = 'none';
+    }
+  });
