ee43a909aad7414c13120b917f7a37e71f5965bc
Joshuabrainiac <156900732+Joshuabrainiac@users.noreply.github.com>
2025-10-12 01:39:19 +0100

    refactor(ui): edit form ui and fix linking
    Edited the form ui to make it more meaningful
    and also change the inputs.
    
    Fixed the sections to make the links show
    their title in the viewport. User can now
    be sure the link brought them to the right
    section in the page.
    
    Closed: #015

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,13 @@ const dropDown = document.querySelector('.hamburger-menu');
 const menuBtn = document.querySelector('.menu-btn');
 const hamburgerLinks = document.querySelectorAll('.close-hamburger');
 
-hamburgerLinks.forEach((element) => {
-  element.addEventListener("click", () => {
+hamburgerLinks.forEach(element => {
+  element.addEventListener('click', () => {
     dropDown.style.display = 'none';
     document.body.style.height = '100%';
     document.body.style.overflow = 'scroll';
-    menuBtn.classList.toggle('active')
-  })
+    menuBtn.classList.toggle('active');
+  });
 });
 
 menuBtn.addEventListener('click', () => {
@@ -28,29 +28,33 @@ menuBtn.addEventListener('click', () => {
   }
 });
 
-
-
 /* Video Script */
-  const openLightbox = document.getElementById('openLightbox');
-  const closeLightbox = document.getElementById('closeLightbox');
-  const lightbox = document.getElementById('lightbox');
-  const popupVideo = document.getElementById('popupVideo');
+const openLightbox = document.getElementById('openLightbox');
+const closeLightbox = document.getElementById('closeLightbox');
+const lightbox = document.getElementById('lightbox');
+const popupVideo = document.getElementById('popupVideo');
 
-  openLightbox.addEventListener('click', () => {
-    lightbox.style.display = 'flex';
-    popupVideo.currentTime = 0; // start fresh
-    popupVideo.play();
-  });
+openLightbox.addEventListener('click', () => {
+  lightbox.style.display = 'flex';
+  popupVideo.currentTime = 0; // start fresh
+  popupVideo.play();
+});
 
-  closeLightbox.addEventListener('click', () => {
+closeLightbox.addEventListener('click', () => {
+  popupVideo.pause();
+  lightbox.style.display = 'none';
+});
+
+// Optional: close lightbox when background is clicked
+lightbox.addEventListener('click', e => {
+  if (e.target === lightbox) {
     popupVideo.pause();
     lightbox.style.display = 'none';
-  });
+  }
+});
 
-  // Optional: close lightbox when background is clicked
-  lightbox.addEventListener('click', (e) => {
-    if (e.target === lightbox) {
-      popupVideo.pause();
-      lightbox.style.display = 'none';
-    }
-  });
+/*----- Hover Script -----*/
+/* document.querySelectorAll('*').forEach(el => {
+  el.addEventListener('touchstart', () => el.classList.add('hover'));
+  el.addEventListener('touchend', () => el.classList.remove('hover'));
+}); */
